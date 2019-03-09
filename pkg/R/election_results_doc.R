70d6b94bee84b1ff5ea5369dc0a1f88b8ecd7c25
Peter Ellis <peter.ellis2013nz@gmail.com>
2019-03-09 12:18:15 +1100

    Bump version and update docs

--- pkg/R/election_results_doc.R
+++ pkg/R/election_results_doc.R
@@ -1,10 +1,11 @@
 #' Australian Federal Election Results by Division
 #' 
-#' Election results by House of Representatives Division (ie seat) 2004 to 2016
+#' Election results by House of Representatives Division (ie seat) 1993 to 2016
 #'
 #' @rdname results_2pp_div
 #' @name results_2pp_div
 #' @docType data
 #' @format A tibble with 750 rows and 14 variables.
-#' @source Australian Electoral Commission
+#' @source Australian Electoral Commission. Results from 2004 onwards are from CSVs in the "Tally room". Earlier
+#' results are taken from the ;-delimited text files in the bulk downloads of zip files (that used to be CD-ROMs)
 NULL
