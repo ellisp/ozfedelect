cb4e92de13bf8a1987c78089ab5082639ae79fe8
Peter Ellis <peter.ellis2013nz@gmail.com>
2019-03-11 08:48:18 +1100

    Add a poll

--- build.R
+++ build.R
@@ -5,14 +5,18 @@ source("setup.R")
 
 # Download and clean polling data from Wikipedia:
 source("prep/collect_polls.R")
+
+# TODO - need some tests here against earlier versions
+
+# More official sources
 source("prep/download-2001-and-earlier.R")
 source("prep/results-2pp-by-division.R")
 source("prep/download-import-boundaries.R")
 source("prep/census-2016-summary-data.R")
 
-# Fit model (takes 20 minutes so beware)
-# source("model-2pp/model-2pp.R")
-
 
 document("pkg")
 check("pkg")
+
+
+
